e5b969ad4611afa9a129ec1d89ea22a8e84da6b0
Jan Gruhnert <jan-gruhnert@startmail.com>
2024-06-22 16:10:32 +0200

    Add links

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# uni-dll-nr2
-Deep Learnin Lab: Multi-Task Learning
+# Deep Learnin Lab: Multi-Task Learning
+
+#### Diffusion Policy:
+- Link to Website: [here](https://diffusion-policy.cs.columbia.edu/)
+
+#### ManiSkill:
+- Link to docs: [here](https://maniskill.readthedocs.io/en/latest/user_guide/index.html)
+
+#### Notes:
+- Link to our notes: [here](https://www.notion.so/Topic-604ddaa66db841af8a176d2de468e3e0)
+
+#### Colab:
+- Link to our Google Colab: [here](https://colab.research.google.com/drive/1XDIHM5ddc5DpQGozea0QeawgApQdqWsj?usp=sharing)
+
+#### Setup
+- Open the Terminal and cd into the git directory
+```
+python -m venv myenv
+source myenv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+```
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import argparse
 
+def main(on_cpu: bool, is_headless: bool, on_server: bool, on_amd: bool):
+    print("cpu:", on_cpu)
+    print("headless:", is_headless)
+    print("server:", on_server)
+    print("amd:", on_amd)
+
+    # Make environment with task_1
+    # Train model_1 on task with diffusion policy
+    # Train model_2 on task without diffusion policy
+    # Validate model_1
+    # Validate model_2
+    # Save model_1
+    # Save model_2
+    # Make environment with task_2
+    # Validate model_1 on task_2
+    # Validate model_2 on task_2
+    # Compare model_1 and model_2
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cpu", dest="cpu", action="store_const", const=True, default=False)
+    parser.add_argument("--headless", dest="headless", action="store_const", const=True, default=False)
+    parser.add_argument("--server", dest="server", action="store_const", const=True, default=False)
+    parser.add_argument("--amd", dest="gpu", action="store_const", const=True, default=False)
+    args = parser.parse_args()
+    main(args.cpu, args.headless, args.server, args.gpu)
