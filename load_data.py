d1d38863271df572026a058000049b9d5274ce52
Jan Gruhnert <jan-gruhnert@startmail.com>
2024-07-04 12:35:52 +0200

    change

--- load_data.py
+++ load_data.py
@@ -1,10 +1,21 @@
 import h5py
 
+dataset_file = ""
 #Open the H5 file in read mode
-with h5py.File('data/PegInsert_ridiculously_small.h5', 'r') as file:
+with h5py.File(dataset_file, 'r') as file:
 	print("Keys: %s" % file.keys())
 	a_group_key = list(file.keys())[0]
 	
 	# Getting the data
 	data = list(file[a_group_key])
 	print(data)
+ 
+    #Get first episode
+	episode = file['traj_0']
+	print("Episode keys: %s" % episode.keys())
+	print("Episode obs: %s" % episode['obs'])
+	print("Episode actions: %s" % episode['actions'])
+	print("Episode terminated: %s" % episode['terminated'])
+	print("Episode truncated: %s" % episode['truncated'])
+
+
