cb2978728531a332f23b71c7cf9e16b58523eacd
Jan Gruhnert <jan-gruhnert@startmail.com>
2024-07-19 15:57:08 +0200

    change

--- data_loading/helper.py
+++ data_loading/helper.py
@@ -1,9 +1,13 @@
-from typing import Union
-import h5py
+# loads h5 data into memory for faster access
+from torch import dtype
 import numpy as np
+import h5py
+from collections import OrderedDict
+from typing import Union
 import torch
 
-# loads h5 data into memory for faster access
+
+
 def load_h5_data(data):
     out = dict()
     for k in data.keys():
@@ -16,7 +20,6 @@ def load_h5_data(data):
 
 def create_sample_indices(episode_ends: np.ndarray, sequence_length: int, pad_before: int = 0, pad_after: int = 0):
     # Currently uses truncated as episode ends which is the end of the episode and not the end of the trajectory
-    # TODO: What to use as episode ends?
     indices = list()
     episode_length = 0
     episode_index = 1 # Start 1 for human readability
@@ -26,7 +29,7 @@ def create_sample_indices(episode_ends: np.ndarray, sequence_length: int, pad_be
             start_idx = 0 if i <= 0 else i - episode_length + 1
             min_start = -pad_before
             max_start = episode_length - sequence_length + pad_after
-            
+
             # Create indices for each possible sequence in the episode
             for idx in range(min_start, max_start + 1):
                 buffer_start_idx = max(idx, 0) + start_idx
@@ -49,7 +52,7 @@ def sample_sequence(train_data, sequence_length, buffer_start_idx, buffer_end_id
         if (sample_start_idx > 0) or (sample_end_idx < sequence_length):
             if isinstance(input_arr, torch.Tensor):
                 data = torch.zeros((sequence_length,) + input_arr.shape[1:], dtype=input_arr.dtype)
-            else: 
+            else:
                 data = np.zeros(shape=(sequence_length,) + input_arr.shape[1:], dtype=input_arr.dtype)
             if sample_start_idx > 0:
                 data[:sample_start_idx] = sample[0]
@@ -68,4 +71,64 @@ def remove_np_uint16(x: Union[np.ndarray, dict]):
                 if x.dtype == np.uint16:
                     return x.astype(np.int32)
                 return x
-            
+
+def convert_observation(obs):
+    return np.concatenate(list(obs.values()), axis=-1)
+
+def get_observations(obs):
+    #ensoure that the observations are in the correct format
+    #and ordered correctly across tasks
+
+    cleaned_obs = OrderedDict()
+    cleaned_obs["qpos"] = obs["agent"]["qpos"]
+    cleaned_obs["qvel"] = obs["agent"]["qvel"]
+    cleaned_obs["tcp_pose"] = obs["extra"]["tcp_pose"]
+
+    #this code is not generic and only works for the specific observation spaces we have
+    if "goal_pose" in obs["extra"]:
+        cleaned_obs["goal_pose"] = obs["extra"]["goal_pose"]
+        obs["extra"].pop("goal_pose")
+    elif "goal_pos" in obs["extra"]:
+        pos = obs["extra"]["goal_pos"]
+        quad = np.array([1, 0, 0, 0])
+        cleaned_obs["goal_pose"] = np.concatenate([pos, quad])
+        obs["extra"].pop("goal_pos")
+    elif "box_hole_pos" in obs["extra"]:
+        cleaned_obs["goal_pose"] = obs["extra"]["box_hole_pos"]
+        obs["extra"].pop("box_hole_pos")
+    elif "cubeB_pose" in obs["extra"]:
+        cleaned_obs["goal_pose"] = obs["extra"]["cubeB_pose"]
+        obs["extra"].pop("cubeB_pose")
+    else:
+        print("No goal pose found in observation")
+        print("Setting goal pose to zero")
+        cleaned_obs["goal_pose"] = np.zeros(7)
+        
+    #is_grasped_reshaped = np.reshape(obs["extra"]["is_grasped"], (len(obs["extra"]["is_grasped"]), 1))
+    # Filter out observations that are no positions
+    for key, value in obs["extra"].items():
+        if value is dtype.is_floating_point:
+            if value.size == 7:
+                cleaned_obs[key] = value
+    
+    return cleaned_obs
+
+def get_data_stats(data):
+    data = data.reshape(-1,data.shape[-1])
+    stats = {
+        'min': np.min(data, axis=0),
+        'max': np.max(data, axis=0)
+    }
+    return stats
+
+def normalize_data(data, stats):
+    # nomalize to [0,1]
+    ndata = (data - stats['min']) / (stats['max'] - stats['min'])
+    # normalize to [-1, 1]
+    ndata = ndata * 2 - 1
+    return ndata
+
+def unnormalize_data(ndata, stats):
+    ndata = (ndata + 1) / 2
+    data = ndata * (stats['max'] - stats['min']) + stats['min']
+    return data
--- data_loading/test.py
+++ data_loading/test.py
@@ -8,8 +8,8 @@ import imageio
 from PIL import Image
 
 # ManiSkill File
-dataset_file1 = "data/trajectory.rgbd.pd_ee_delta_pos.h5"
-dataset_file2 = "data/trajectory.rgbd.pd_ee_delta_pose.h5"
+dataset_file1 = "data/staterawdata.state_dict.pd_ee_delta_pos.h5"
+
 
 load_count = 20  # Load all episodes
 succes_only = False  # Load all episodes regardless of success
@@ -33,56 +33,12 @@ dataset = StateNormalDataset(
     obs_horizon,
     action_horizon,
     load_count,
-    succes_only,
-    normalize,
 )
-
-dataset1 = StreamingTrajectoryDataset(
-    dataset_file2, pred_horizon, obs_horizon, action_horizon
-)
-
-dataset2 = StreamingTrajectoryDataset2(
-    dataset_file2, pred_horizon, obs_horizon, action_horizon
-)
-
-# Debugging
-# Dataset should be 3dim (batch, sequence, features)
-# Can be used by Diffusion policy
 
 dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
-dataloader2 = DataLoader(dataset2, batch_size=10, num_workers=1, drop_last=drop_last)
 
-batch_count = 10
-iterator = iter(dataloader)
-pil_images = []
-while batch_count > 0:
-    batch_count -= 1
-    batch = next(iterator)
-    
-    for i in range(batch["obs_base_camera_rgb"].shape[0]):
-        sequence = batch["obs_base_camera_rgb"][i]
-        for j in range(sequence.shape[0]):
-            pil_images.append(Image.fromarray(sequence[j].numpy().astype(np.uint8)))
-        #print(sequence.shape)
-        #pil_images.append(Image.fromarray(sequence[0].numpy().astype(np.uint8)))
+batch = next(iter(dataloader))
 
-imageio.mimsave(f"outputs/sequencemani3.gif", pil_images, fps=30)
-        
-    
+print(batch["obs"].shape)
+print(batch["actions"].shape)
 
-batch_count2 = 10
-iterator2 = iter(dataloader2)
-pil_images2 = []
-while batch_count2 > 0:
-    batch_count2 -= 1
-    batch = next(iterator2)
-    
-    for i in range(batch["rgbd"].shape[0]):
-        sequence = batch["rgbd"][i]
-        for j in range(sequence.shape[0]):
-            image = sequence[j][ :, :, :3].numpy().astype(np.uint8)
-            print(image.shape)
-            pil_images2.append(Image.fromarray(image))
-        
-imageio.mimsave(f"outputs/sequencemani2.gif", pil_images2, fps=30)
-      
